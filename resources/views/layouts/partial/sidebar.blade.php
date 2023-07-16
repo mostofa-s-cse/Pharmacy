02e1f8ddb3a4551462fd63b13e3fa7f03051f557
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-07-16 15:52:48 +0600

    side menu set

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -5,36 +5,157 @@
                         <li class="menu-title">
                             <span>Main</span>
                         </li>
+
                         <li class="submenu">
-                            <a href="{{ url('admin/dashboard')}}"><i class="la la-dashboard"></i> <span> Dashboard Admin</span> <span
+                            <a href="#" class="noti-dot"><i class="la la-home"></i> <span> Dashboard </span> <span
                                     class="menu-arrow"></span></a>
                             <ul style="display: none;">
-                                <li><a class="active" href="{{ url('admin/dashboard')}}">Admin Dashboard</a></li>
-                                <li><a href="{{ url('admin/dashboard')}}">Employee Dashboard</a></li>
+                                <li><a href="{{ url('admin/products')}}">Dashboard</a></li>
                             </ul>
                         </li>
-                        <!-- <li class="menu-title">
-                            <span>Employees</span>
-                        </li> -->
+
                         <li class="submenu">
-                            <a href="#" class="noti-dot"><i class="la la-cube"></i> <span> Product </span> <span
+                            <a href="#"><i class="la la-layer-group"></i> <span> Categories </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">All Categories</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-box"></i> <span> Product </span> <span
                                     class="menu-arrow"></span></a>
                             <ul style="display: none;">
                                 <li><a href="{{ url('admin/products')}}">All Product</a></li>
-                                <li><a href="holidays.html">Holidays</a></li>
-                                <li><a href="leaves.html">Leaves (Admin) <span
-                                            class="badge rounded-pill bg-primary float-end">1</span></a></li>
-                                <li><a href="leaves-employee.html">Leaves (Employee)</a></li>
-                                <li><a href="leave-settings.html">Leave Settings</a></li>
-                                <li><a href="attendance.html">Attendance (Admin)</a></li>
-                                <li><a href="attendance-employee.html">Attendance (Employee)</a></li>
-                                <li><a href="departments.html">Departments</a></li>
-                                <li><a href="designations.html">Designations</a></li>
-                                <li><a href="timesheet.html">Timesheet</a></li>
-                                <li><a href="shift-scheduling.html">Shift & Schedule</a></li>
-                                <li><a href="overtime.html">Overtime</a></li>
                             </ul>
                         </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-star-o"></i> <span> Purchase </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Purchase Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-chart-bar"></i> <span> Sales </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Sales Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-dice-d6"></i> <span>Stock</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Stock  Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-exclamation-triangle"></i> <span> Damage  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Damage  Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-city"></i> <span> Company  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Company  Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-people-carry"></i> <span> Supplier  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Supplier  Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-user-tag"></i> <span> Customer  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Customer  Details</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-qrcode"></i> <span> Barcode & Scanning  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Barcode Generation & Scanning</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-user-alt"></i> <span> Accounts  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Accounts Details</a></li>
+                                <li><a href="{{ url('admin/products')}}">Billing History</a></li>
+                                <li><a href="{{ url('admin/products')}}">Other Transaction</a></li>
+                                <li><a href="{{ url('admin/products')}}">Transaction History</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-file-alt"></i> <span> Report  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Sale Report</a></li>
+                                <li><a href="{{ url('admin/products')}}">Purchase Report</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-file-invoice-dollar"></i> <span> Cash Memo  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Cash Memo</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-lock"></i> <span> Access Control  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Permissions</a></li>
+                                <li><a href="{{ url('admin/products')}}">Roles</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-users"></i> <span> User  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">All Users</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-user-circle"></i> <span> Profile  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Profile Edit</a></li>
+                                <li><a href="{{ url('admin/products')}}"><i class="material-icons" style="font-size:20px;">backup</i> <span>Backups</span></a></li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#"><i class="la la-cog"></i> <span> Settings  </span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="{{ url('admin/products')}}">Settings</a></li>
+                            </ul>
+                        </li>
+
                     </ul>
                 </div>
             </div>
